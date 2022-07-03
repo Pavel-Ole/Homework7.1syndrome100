3ba0ee72e004389d756aaaf270609a33bfe3294e
Pavel O <pavelnitel@gmail.com>
2022-07-03 21:11:29 +0300

    Coating 100%

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,4 +1,4 @@
-
+package ru.netology.statistic;
 
 public class StatisticsService {
     /**
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,4 +1,8 @@
+package ru.netology.statistic;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatisticsServiceTest {
     @Test
@@ -7,6 +11,18 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAnotherMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 13, 11, 11, 12};
+        long expected = 13;
 
         long actual = service.findMax(incomesInBillions);
 
